8672dfcf10f7cb38dc047a2663636be379206f8a
songbing <songbing@ynsy.com>
2018-04-10 14:25:53 +0800

    redis 查询乱码功能

--- config/redis_con.py
+++ config/redis_con.py
@@ -6,5 +6,6 @@ import redis
 def redis_cli(host,port):
     try:
         return redis.Redis(host=host,port=port,db=0)##如果设置了密码，就加上password=密码
+        #return redis.StrictRedis(host=host, port=port, decode_responses=True)##如果设置了密码，就加上password=密码
     except redis.ConnectionError as rediserror:
         return (rediserror)
